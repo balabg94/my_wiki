2046306657aa486c0d3d7df49aed53d20941889a
Balagovind <bala@hamon.in>
2020-08-06 15:23:00 +0530

    Adds Error handling.

--- wiki.py
+++ wiki.py
@@ -13,10 +13,13 @@ def save_page(title, content):
 def load_page(title):
 	file_name=title+'.txt'
 	file_path=os.path.join(pages_path, file_name)
-	
-	f=open(file_path, 'r')
-	content= f.read()
-	
+	try:
+		f=open(file_path, 'r')
+		content= f.read()
+		1+'2'
+	except FileNotFoundError:		
+		content="No information on this topic"
+		
 	return content
 
 def list_topics():
