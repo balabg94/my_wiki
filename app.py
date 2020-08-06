9ee072095a5172a7f7cae72fefa7fb67f24e3d5a
Suroor Hussain <suroorhussain@hamon.in>
2020-08-06 20:50:48 +0530

    Display Python page on the browser

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
 
+import wiki
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/Python')
+def view_page():
+    title = 'Python'
+    content = wiki.load_page(title)
+    return render_template('page.html', page_content=content, page_title=title)
 
 if __name__ == '__main__':
     app.run(debug=True)
