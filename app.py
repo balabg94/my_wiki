009e8ee9abf20241190a0a17468e0a670b5b58f4
RohithSml <rohith.sml94@gmail.com>
2020-08-08 00:49:39 +0530

    Fixes: @route trailing slashes.

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ def home():
     titles = wiki.list_topics()
     return render_template('home.html', page_titles=titles)
 
-@app.route('/<title>')
+@app.route('/<title>/')
 def view_page(title):
     content = wiki.load_page(title)
     return render_template('page.html', page_title=title, page_content=content)
 
-@app.route('/write', methods=['GET', 'POST'])
+@app.route('/write/', methods=['GET', 'POST'])
 def write_page():
     if request.method == 'GET':
         return render_template('write.html')
